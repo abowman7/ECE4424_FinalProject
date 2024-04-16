e85b8e26ee5b539e0da653bb0f9075f5194deb21
Nii Laryea <niil@vt.edu>
2024-04-15 20:16:12 -0400

    Adjusted incorrectly formatted data points causing errors, updated gym capacity value and added print statements

--- learner.py
+++ learner.py
@@ -10,10 +10,13 @@ targetPath = "mccomasTarget.csv"    #target file path
 initData = np.loadtxt(dataPath, delimiter=",", dtype=str, encoding="utf-8-sig")  
 initTarget = np.loadtxt(targetPath, delimiter=",", dtype=str, encoding="utf-8-sig")
 
+#-----------------Getting data ready for processing----------------
+
 # save temps as floats (string -> float)
 floatTemp = []
 for temp in initData:
     floatTemp.append(float(temp[0]))
+#print(floatTemp)
 
 # save times as floats (string -> float)
 floatTime = []
@@ -22,21 +25,25 @@ for time in initData:
     hour = float(breaked[0])
     minutes = float(breaked[1])
     # time saved as hour.(minute/60)
-    floatTime.append((hour + (minutes / 60.0)))
+    floatTime.append(hour + round((minutes / 60.0), 2))
+#print(floatTime)
 
 # save day of week as float (string -> float) 0 is Monday, 6 is Sunday
 floatDay = []
 for day in initData:
     floatDay.append(float(day[3]))
+#print(floatDay)
 
-# make occupancy a bool of < half full capacity (650)
-threshold = 650.0 / 2.0 # half of full capacity
+# make occupancy a bool of < half full capacity (600)
+threshold = 600.0 / 2.0 # half of full capacity
 occupancy = []
 for ppl in initTarget:
     occupancy.append((float(ppl[0]) < threshold))   #turn to bool (0/1) for classification
+#print(occupancy)
 
 #put input data into a single array/list
 inputData = []
 for i in range(len(floatTime)):
     inputData.append([floatTemp[i], floatTime[i], floatDay[i]])
-
+    
+print(inputData)
