5e4387c5125609297d9df9bfa13a925e5bef2659
Adam Bowman <abowman7@vt.edu>
2024-04-16 14:19:59 -0400

    Fix mine

--- learner.py
+++ learner.py
@@ -17,10 +17,13 @@ targetPath = "mccomasTarget.csv"    #target file path
 initData = np.loadtxt(dataPath, delimiter=",", dtype=str, encoding="utf-8-sig")  
 initTarget = np.loadtxt(targetPath, delimiter=",", dtype=str, encoding="utf-8-sig")
 
+#-----------------Getting data ready for processing----------------
+
 # save temps as floats (string -> float)
 floatTemp = []
 for temp in initData:
     floatTemp.append(float(temp[0]))
+#print(floatTemp)
 
 # save times as floats (string -> float)
 floatTime = []
@@ -29,24 +32,91 @@ for time in initData:
     hour = float(breaked[0])
     minutes = float(breaked[1])
     # time saved as hour.(minute/60)
-    floatTime.append((hour + (minutes / 60.0)))
+    floatTime.append(hour + round((minutes / 60.0), 2))
+#print(floatTime)
 
 # save day of week as float (string -> float) 0 is Monday, 6 is Sunday
 floatDay = []
 for day in initData:
     floatDay.append(float(day[3]))
+#print(floatDay)
 
-# make occupancy a bool of < half full capacity (650)
-threshold = 650.0 / 2.0 # half of full capacity
+# make occupancy a bool of < half full capacity (600)
+threshold = 600.0 / 2.0 # half of full capacity
 occupancy = []
 for ppl in initTarget:
     occupancy.append((float(ppl[0]) < threshold))   #turn to bool (0/1) for classification
+#print(occupancy)
 
 #put input data into a single array/list
 inputData = []
 for i in range(len(floatTime)):
     inputData.append([floatTemp[i], floatTime[i], floatDay[i]])
+#print(inputData)
 
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
+# Splitting into training and test data
+X_train, X_test, y_train, y_test = train_test_split(inputData, occupancy, test_size=0.5, random_state=0)
+#print(X_train)
+
 #gnb = GaussianNB()
 #y_pred = gnb.fit(X_train, y_train).predict(X_test)
+
+
+# Applying KNN Classification 
+
+#knn using ball tree algorithm
+bestBallAccuracy = 0
+bestBallKValue = 0
+BallAccuracy = []
+
+for i in range (1, 31):
+    knn_ball = KNeighborsClassifier(n_neighbors=i, algorithm='ball_tree')    
+    knn_ball.fit(X_train, y_train)
+    mean_accuracy = knn_ball.score(X_test, y_test)
+    BallAccuracy.append(mean_accuracy)
+    #print(mean_accuracy)
+
+    if bestBallAccuracy < mean_accuracy :
+        bestBallAccuracy = mean_accuracy
+        bestBallKValue = i
+        
+print("Using the ball tree algorithm with knn, the best k value was {}, with a mean accuracy of {}".format(bestBallKValue, bestBallAccuracy))
+
+
+#knn using kd tree algorithm
+bestKdAccuracy = 0
+bestKdKValue = 0
+KdAccuracy = []
+
+for i in range (1, 31):
+    knn_kd = KNeighborsClassifier(n_neighbors=i, algorithm='kd_tree')    
+    knn_kd.fit(X_train, y_train)
+    mean_accuracy = knn_kd.score(X_test, y_test)
+    KdAccuracy.append(mean_accuracy)
+    #print(mean_accuracy)
+
+    if bestKdAccuracy < mean_accuracy :
+        bestKdAccuracy = mean_accuracy
+        bestKdKValue = i
+        
+print("Using the kd tree algorithm with knn, the best k value was {}, with a mean accuracy of {}".format(bestKdKValue, bestKdAccuracy))
+
+
+#knn using brute force search
+bestBruteAccuracy = 0
+bestBruteKValue = 0
+BruteAccuracy = []
+
+for i in range (1, 31):
+    knn_brute = KNeighborsClassifier(n_neighbors=i, algorithm='brute')    
+    knn_brute.fit(X_train, y_train)
+    mean_accuracy = knn_brute.score(X_test, y_test)
+    BruteAccuracy.append(mean_accuracy)
+    #print(mean_accuracy)
+
+    if bestBruteAccuracy < mean_accuracy :
+        bestBruteAccuracy = mean_accuracy
+        bestBruteKValue = i
+        
+
+print("Using the brute-force search algorithm with knn, the best k value was {}, with a mean accuracy of {}".format(bestBruteKValue, bestBruteAccuracy))
